8806bbb1139c61767c4c8b3b7a5d75ce18f9633b
Sebastien Voillot <sebastienvoillot@gmail.com>
2025-08-20 16:40:08 +0200

    migration vers alwaysdata

--- actions/dbserveur/db.php
+++ actions/dbserveur/db.php
@@ -1,8 +1,13 @@
 <?php
-            $dbname = "09007_ressourceb";
+            /* $dbname = "09007_ressourceb";
             $serveur = "sql01.ouvaton.coop";
             $login = "09007_ressourceb";
-            $pass = "LaRessourcerieDeBrie77170!";
+            $pass = "LaRessourcerieDeBrie77170!"; */
+
+            $dbname = "ressourcebrie_bdd";
+            $serveur ="mysql-ressourcebrie.alwaysdata.net";
+            $login = "418153";
+            $pass = "geMsos-wunxoc-1fucbu";
             
             try{
                         $db = new PDO("mysql:host=$serveur;dbname=$dbname;charset=utf8;", $login, $pass);
